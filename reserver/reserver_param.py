9e45352106d449382e1d0d44f2fb999ce71631e9
AHReccese <amirhssin6rst@gmail.com>
2024-01-20 00:11:39 +0330

    add package name to the params

--- reserver/reserver_param.py
+++ reserver/reserver_param.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Parameters and constants."""
 RESERVER_VERSION = "0.1"
+RESERVER_NAME = "reserver"
 
 PYPI_TEST_URL = "https://test.pypi.org/project"
 PYPI_MAIN_URL = "https://pypi.org/project"
