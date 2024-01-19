f49afe55c75d3ed0288ceda6bf79b38a0550054c
AHReccese <amirhssin6rst@gmail.com>
2024-01-19 23:49:26 +0330

    add `is_platform_linux` & `get_random_name` utility function

---
+++ reserver/util.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Utility functions."""
+
+from platform import system
+from hashlib import sha256
+from time import time
+
+def is_platform_linux():
+    """
+    Check whether current platform is linux or not.
+
+    :return: bool
+    """
+    return system() == "Linux"
+
+def get_random_name():
+    """
+    Generate a random str based on current timestamp.
+
+    :return: str
+    """
+    return sha256(str(time()).encode("utf-8")).hexdigest()
