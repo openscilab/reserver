e8e7e0710f00512f7fc8197db22c3b388e65e6d4
AHReccese <amirhssin6rst@gmail.com>
2024-01-19 23:47:03 +0330

    add docstring for functions in `reserver_func.py`

--- reserver/reserver_func.py
+++ reserver/reserver_func.py
@@ -4,6 +4,15 @@ from requests import get
 from .reserver_param import PYPI_TEST_URL, PYPI_MAIN_URL
 
 def does_package_exist(suggested_name, test_pypi):
+    """
+    Checks whether a package with the given name exists or not.
+
+    :param suggested_name: given name to search in pypi(or test.pypi)
+    :type suggested_name: str
+    :param test_pypi: indicates to search in test.pypi or not
+    :type test_pypi: bool
+    :return: whether given name does exist in the pypi or not(as a boolean value)
+    """
     if not isinstance(suggested_name, str):
         suggested_name = str(suggested_name)
     if test_pypi:
@@ -14,6 +23,13 @@ def does_package_exist(suggested_name, test_pypi):
     return not response.status_code == 404
 
 def generate_template_setup_py(package_name):
+    """
+    Generates a template `setup.py` file for given package name.
+
+    :param package_name: given name to generate template `setup.py` for it.
+    :type package_name: str
+    :return: None
+    """
     setup_py_content = """
 import sys
 
