c19d101118642e56b038120709d6a3f6be180c5f
AHReccese <amirhssin6rst@gmail.com>
2024-01-19 23:35:26 +0330

    add function to check given name existance as a package name

--- reserver/reserver_func.py
+++ reserver/reserver_func.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
 """Functions."""
+from requests import get 
+from .reserver_param import PYPI_TEST_URL, PYPI_MAIN_URL
+
+def does_package_exist(suggested_name, test_pypi):
+    if not isinstance(suggested_name, str):
+        suggested_name = str(suggested_name)
+    if test_pypi:
+        url = PYPI_TEST_URL + "/" + suggested_name + "/"
+    else:
+        url = PYPI_MAIN_URL + "/" + suggested_name + "/"
+    response = get(url)
+    return not response.status_code == 404
