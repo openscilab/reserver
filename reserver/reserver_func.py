4e0a4cc43e1b20d0a9d9dd7134bc4f624097278d
AHReccese <amirhssin6rst@gmail.com>
2024-01-24 22:55:03 +0330

    update template `setup.py`

--- reserver/reserver_func.py
+++ reserver/reserver_func.py
@@ -60,26 +60,34 @@ setup(
     name =""" + "\"" + package_name + "\"" + """,
     packages=[],
     version='0.0.0',
-    description='DESCRIPTION',
-    long_description="LONG_DESCRIPTION",
+    description='This name has been reserved using Reserver',
+    long_description=\"\"\"
+    This name has been reserved using [Reserver](https://github.com/openscilab/reserver).
+    \"\"\",
     long_description_content_type='text/markdown',
     author='Development Team',
     author_email='test@test.com',
     url='https://url.com',
     download_url='https://download_url.com',
-    keywords="python3 python",
+    keywords="python3 python reserve reserver reserved",
     project_urls={
             'Source': 'https://github.com/source',
     },
     install_requires="",
     python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
+        \'Development Status :: 1 - Planning\',
+        \'Programming Language :: Python :: 3.6\',
+        \'Programming Language :: Python :: 3.7\',
+        \'Programming Language :: Python :: 3.8\',
+        \'Programming Language :: Python :: 3.9\',
+        \'Programming Language :: Python :: 3.10\',
+        \'Programming Language :: Python :: 3.11\',
+        \'Programming Language :: Python :: 3.12\',
     ],
     license='MIT',
 )
 
-    """
+"""
     with open(package_name + "_setup.py", "w+") as f:
         f.writelines(setup_py_content)
