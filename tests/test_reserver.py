932de1170d881aa6f2e9eee6971519ddd3502cba
AHReccese <amirhssin6rst@gmail.com>
2024-01-29 09:10:47 +0330

    update test cases

--- tests/test_reserver.py
+++ tests/test_reserver.py
@@ -5,17 +5,17 @@ test_pypi_token = os.environ.get("TEST_PYPI_PASSWORD")
 
 def test_package_exists():
     # test reserved name
-    uploader = Uploader(test_pypi_token, is_test_pypi_account= True)
+    uploader = Uploader(test_pypi_token, test_pypi= True)
     assert uploader.upload_to_pypi("numpy") == False
 
 def test_valid_package_invalid_credentials():
     # test not reserved name -> wrong credentials
     wrong_pypi_token = "pypi-wrong-api-token"
-    uploader = Uploader(wrong_pypi_token, is_test_pypi_account= True)
+    uploader = Uploader(wrong_pypi_token, test_pypi= True)
     assert uploader.upload_to_pypi(get_random_name()) == False
 
 def test_valid_package_valid_credentials():
     # test not reserved name -> correct credentials
-    # uploader = Uploader(test_pypi_token, is_test_pypi_account= True)
+    # uploader = Uploader(test_pypi_token, test_pypi= True)
     # uploader.upload_to_pypi(get_random_name())
     assert True == True
