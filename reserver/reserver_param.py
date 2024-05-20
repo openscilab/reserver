f634b57269707ccbd97af7d8dd32086119991c24
AHReccese <amirhssin6rst@gmail.com>
2024-05-21 02:01:16 +0330

    updated

--- reserver/reserver_param.py
+++ reserver/reserver_param.py
@@ -2,10 +2,8 @@
 """Parameters and constants."""
 RESERVER_VERSION = "0.1"
 RESERVER_NAME = "reserver"
-
 PYPI_TEST_URL = "https://test.pypi.org/project"
 PYPI_MAIN_URL = "https://pypi.org/project"
-
 PACKAGE_PARAMETERS = {
     "description": "This name has been reserved using Reserver",
     "author": "Development Team",
@@ -15,7 +13,6 @@ PACKAGE_PARAMETERS = {
     "source": "https://github.com/source",
     "license": "MIT",
 }
-
 VALIDATIONS = {
     "email": r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
     "url": r'^(http|https)://[a-zA-Z0-9.-_]+\.[a-zA-Z]{2,}(/\S*)?$',
