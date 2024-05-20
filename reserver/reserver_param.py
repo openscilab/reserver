55f0adc9ee4bd53b0021e68b041fd3581068724f
AHReccese <amirhssin6rst@gmail.com>
2024-05-21 01:42:08 +0330

    add customizable package parameter + validation regex for `email` & `url`

--- reserver/reserver_param.py
+++ reserver/reserver_param.py
@@ -5,3 +5,18 @@ RESERVER_NAME = "reserver"
 
 PYPI_TEST_URL = "https://test.pypi.org/project"
 PYPI_MAIN_URL = "https://pypi.org/project"
+
+PACKAGE_PARAMETERS = {
+    "description": "This name has been reserved using Reserver",
+    "author": "Development Team",
+    "author_email": "test@test.com",
+    "url": "https://url.com",
+    "download_url": "https://download_url.com",
+    "source": "https://github.com/source",
+    "license": "MIT",
+}
+
+VALIDATIONS = {
+    "email": r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+    "url": r'^(http|https)://[a-zA-Z0-9.-_]+\.[a-zA-Z]{2,}(/\S*)?$',
+}
