ab9e73d936f6edd0e4f31e495056008fe09e50b3
AHReccese <amirhssin6rst@gmail.com>
2024-06-11 16:42:25 +0330

    enhance CLI

--- reserver/__main__.py
+++ reserver/__main__.py
@@ -16,23 +16,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--name',
-        nargs='+',
+        nargs='?',
+        type=str,
+        metavar="PACKAGE_NAME",
         help='Name(s) to get reserved',
-        required=True
     )
     parser.add_argument(
         '--token',
+        nargs='?',
+        type=str,
+        metavar="(TEST.PYPI|PYPI)_TOKEN",
         help='The token for (main|test) PyPI account',
-        required=True
     )
-    parser.add_argument('--test', action='store_true', help='Flag identifying test account or not')
+    parser.add_argument('--test', action='store_true', help='Flag identifying test account or not', default=False)
     parser.add_argument('--version', help="version", action='store_true', default=False)
     parser.add_argument('-v', help="version", action='store_true', default=False)
     args = parser.parse_known_args()[0]
     if args.version or args.v:
         print(RESERVER_VERSION)
     names = args.name
-    test_pypi = True if args.test else False
+    test_pypi = args.test
     pypi_token = args.token
     if names and pypi_token:
         PyPIUploader(pypi_token, test_pypi).batch_upload(names)
