43fdb2d99232135c346088ea98bc5c745dec9026
AHReccese <amirhssin6rst@gmail.com>
2024-09-18 13:09:45 -0400

    Reserver Error Types added in `reserver/__init__.py`

--- reserver/__init__.py
+++ reserver/__init__.py
@@ -2,5 +2,6 @@
 """Reserver modules."""
 from .reserver_param import RESERVER_VERSION
 from .reserver_obj import PyPIUploader
+from .reserver_errors import ReserverBaseError
 
 __version__ = RESERVER_VERSION
