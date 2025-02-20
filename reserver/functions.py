8439bf159a761cd5641c3c68b35b867323e4c1e7
AHReccese <amirhssin6rst@gmail.com>
2025-02-20 10:51:06 -0500

    add generate `pyproject.toml` instead of `setup.py`

--- reserver/functions.py
+++ reserver/functions.py
@@ -2,8 +2,8 @@
 """Reserver functions."""
 import re
 from time import time
+from pathlib import Path
 from hashlib import sha256
-from os import mkdir, rmdir
 from .params import PACKAGE_PARAMETERS, VALIDATIONS, OVERVIEW
 from .params import INVALID_PACKAGE_PARAMETER_NAME_ERROR, INVALID_PACKAGE_PARAMETER_VALUE_ERROR
 from .errors import ReserverBaseError
@@ -43,69 +43,57 @@ def get_package_parameter(parameter, user_parameters, regex=None):
     return user_parameters[parameter]
 
 
-def generate_template_setup_py(package_name, user_parameters):
+def generate_template_pyproject_toml(package_name, user_parameters):
     """
-    Generate a template `setup.py` file for given package name.
+    Generate a template `pyproject.toml` file for given package name.
 
-    :param package_name: given name to generate template `setup.py` for it.
+    :param package_name: given name to generate template `pyproject.toml` for it.
     :type package_name: str
     :return: None
     """
-    setup_py_content = """
-import sys
+    pyproject_toml_content = """
+[build-system]
+requires = ["setuptools"]
+build-backend = "setuptools.build_meta"
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+[project]
+name = """ + "\"" + package_name + "\"" + """
+version = "0.0.0"
+description = """ + "\"" + get_package_parameter("description", user_parameters) + "\"" + """
+readme = { text = "This name has been reserved using [Reserver](https://github.com/openscilab/reserver).", content-type = "text/markdown" }
+authors = [
+    { name = """ + "\"" + get_package_parameter("author", user_parameters) + "\"" + """, email = """ + "\"" + get_package_parameter("author_email", user_parameters, "email") + "\"" + """ }
+]
+license = { text = """ + "\"" + get_package_parameter("license", user_parameters) + "\"" + """ }
+keywords = ["python3", "python", "reserve", "reserver", "reserved"]
+classifiers = [
+    "Development Status :: 1 - Planning",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+]
+dependencies = []  # Explicitly specify no dependencies
+requires-python = ">=3.6"
 
-# invalid email
-# download url
-# url
-# project urls
+[project.urls]
+Homepage = """ + "\"" + get_package_parameter("url", user_parameters, "url") + "\"" + """
+Download = """ + "\"" + get_package_parameter("download_url", user_parameters, "url") + "\"" + """
+Source = """ + "\"" + get_package_parameter("source", user_parameters, "url") + "\"" + """
 
-setup(
-    name =""" + "\"" + package_name + "\"" + """,
-    packages=[""" + "\"" + package_name + "\"" + "," + """],
-    version='0.0.0',
-    description=""" + "\"" + get_package_parameter("description", user_parameters) + "\"" + """,
-    long_description= \"This name has been reserved using [Reserver](https://github.com/openscilab/reserver).\",
-    long_description_content_type='text/markdown',
-    author=""" + "\"" + get_package_parameter("author", user_parameters) + "\"" + """,
-    author_email=""" + "\"" + get_package_parameter("author_email", user_parameters, "email") + "\"" + """,
-    url=""" + "\"" + get_package_parameter("url", user_parameters, "url") + "\"" + """,
-    download_url=""" + "\"" + get_package_parameter("download_url", user_parameters, "url") + "\"" + """,
-    keywords="python3 python reserve reserver reserved",
-    project_urls={
-            'Source':""" + "\"" + get_package_parameter("source", user_parameters, "url") + "\"" + """,
-    },
-    install_requires="",
-    python_requires='>=3.6',
-    classifiers=[
-        \'Development Status :: 1 - Planning\',
-        \'Programming Language :: Python :: 3.6\',
-        \'Programming Language :: Python :: 3.7\',
-        \'Programming Language :: Python :: 3.8\',
-        \'Programming Language :: Python :: 3.9\',
-        \'Programming Language :: Python :: 3.10\',
-        \'Programming Language :: Python :: 3.11\',
-        \'Programming Language :: Python :: 3.12\',
-    ],
-    license=""" + "\"" + get_package_parameter("license", user_parameters) + "\"" + """,
-)
+[tool.setuptools.packages.find]
+where = ["."]
+include = [""" + "\"" + package_name + "\"" + """]
 
 """
-    with open(package_name + "_setup.py", "w+") as f:
-        f.writelines(setup_py_content)
+    package_path = Path(package_name)
+    package_path.mkdir(parents=True, exist_ok=True)
 
-    try:
-        mkdir(package_name)
-    except FileExistsError:
-        rmdir(package_name)
-        mkdir(package_name)
-    with open(package_name + "/__init__.py", "w") as f:
-        f.write("# -*- coding: utf-8 -*-\n")
-        f.write("\"\"\"" + package_name + " modules." + "\"\"\"")
+    with open(package_path / "pyproject.toml", "w+", encoding="utf-8") as f:
+        f.writelines(pyproject_toml_content)
 
 
 def reserver_help():
