dc7007e4cdd638f76fb9bc2eb1dc0e53f8cddd16
AHReccese <amirhssin6rst@gmail.com>
2025-02-24 17:56:54 -0500

    refactor and put back `generate_template_setup_py` function

--- reserver/functions.py
+++ reserver/functions.py
@@ -43,6 +43,69 @@ def get_package_parameter(parameter, user_parameters, regex=None):
     return user_parameters[parameter]
 
 
+def generate_template_setup_py(package_name, user_parameters):
+    """
+    Generate a template `setup.py` file for given package name.
+
+    :param package_name: given name to generate template `setup.py` for it.
+    :type package_name: str
+    :return: None
+    """
+    setup_py_content = """
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# invalid email
+# download url
+# url
+# project urls
+
+setup(
+    name =""" + "\"" + package_name + "\"" + """,
+    packages=[""" + "\".\"" + "," + """],
+    version='0.0.0',
+    description=""" + "\"" + get_package_parameter("description", user_parameters) + "\"" + """,
+    long_description= \"This name has been reserved using [Reserver](https://github.com/openscilab/reserver).\",
+    long_description_content_type='text/markdown',
+    author=""" + "\"" + get_package_parameter("author", user_parameters) + "\"" + """,
+    author_email=""" + "\"" + get_package_parameter("author_email", user_parameters, "email") + "\"" + """,
+    url=""" + "\"" + get_package_parameter("url", user_parameters, "url") + "\"" + """,
+    download_url=""" + "\"" + get_package_parameter("download_url", user_parameters, "url") + "\"" + """,
+    keywords="python3 python reserve reserver reserved",
+    project_urls={
+            'Source':""" + "\"" + get_package_parameter("source", user_parameters, "url") + "\"" + """,
+    },
+    install_requires="",
+    python_requires='>=3.6',
+    classifiers=[
+        \'Development Status :: 1 - Planning\',
+        \'Programming Language :: Python :: 3.6\',
+        \'Programming Language :: Python :: 3.7\',
+        \'Programming Language :: Python :: 3.8\',
+        \'Programming Language :: Python :: 3.9\',
+        \'Programming Language :: Python :: 3.10\',
+        \'Programming Language :: Python :: 3.11\',
+        \'Programming Language :: Python :: 3.12\',
+    ],
+    license=""" + "\"" + get_package_parameter("license", user_parameters) + "\"" + """,
+)
+
+"""
+    package_path = Path(package_name)
+    package_path.mkdir(parents=True, exist_ok=True)
+
+    with open(package_path / "setup.py", "w+", encoding="utf-8") as f:
+        f.writelines(setup_py_content)
+
+    with open(package_path / "__init__.py", "w+", encoding="utf-8") as f:
+        f.write("# -*- coding: utf-8 -*-\n")
+        f.write("\"\"\"" + package_name + " modules." + "\"\"\"")
+
+
 def generate_template_pyproject_toml(package_name, user_parameters):
     """
     Generate a template `pyproject.toml` file for given package name.
